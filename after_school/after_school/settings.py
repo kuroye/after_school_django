f4ae4a5e6a16caeda89fc222935fdc7ec570a568
kuroye <erhanaket77@gmail.com>
2023-11-24 19:56:17 +0600

    new super user

--- after_school/after_school/settings.py
+++ after_school/after_school/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-*4_9-1o()*kad&#0ub8ab&8$(1$n9r!xqh&a(8r$vw$qbptl6n
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['192.168.0.5', '25.51.91.61',  '127.0.0.1']
 
 
 # Application definition
