f4dd1e90a1036c074ca00202085d33a0a9a6581f
kuroye <erhanaket77@gmail.com>
2023-12-28 21:37:10 +0600

    club page

--- after_school/club/views.py
+++ after_school/club/views.py
@@ -6,8 +6,21 @@ from main.models import *
 def display(request):
     player = Character.objects.get(is_player=1)
 
+    character_item = CharacterItem.objects.filter(character=player).first()
+
+    items = []
+
+    my_items = character_item.item.all()
+
+    for i in range(player.max_item):
+        if i >= len(my_items.values()):
+            items.append({})
+        else:
+            items.append(my_items.values()[i])
+
     context = {
         "player" : player,
+        "items": items,
         }
 
     return render(request, 'club.html', context)
